aa9e6230ef6f8c799ff7977ed930c50da97a730b
Nikita Lutsai <105123083+nikitalutsai@users.noreply.github.com>
2023-01-14 13:45:41 +0100

    Take out comments

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fpdf import FPDF
 import pandas as pd
 
-# Create an instance from a fpdf class
 pdf = FPDF(orientation="P", unit="mm", format="A4")
-# Set that pages should not be broken automatically.
 pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv("topics.csv")
 
@@ -12,22 +10,12 @@ for index, row in df.iterrows():
         pdf.add_page()
 
         pdf.set_font(family="Times", style="B", size=15)
-        # Set a color to a text (In parameters, should put a RGB values)
         pdf.set_text_color(254, 100, 100)
-        # Set a cell
         pdf.cell(w=0, h=15, txt=row["Topic"], align="L", ln=1)
-        # Set a line
-        # x1: coordinates a distance from beginning of line (left to right)
-        # y2: coordinates from end point of line on the left (from top to line)
-        # x2: lenght of the lin
-        # y2: coordinates from end point of line on the right (from top to line)
+
         for y in range(20, 280, 10):
             pdf.line(10, y, 200, y)
 
-
-
-        # Set the footer!
-        # Create a line and create 270mm of break lines (setting up high)
         pdf.ln(260)
         pdf.set_font(family="Times", style="I", size=10)
         pdf.set_text_color(180, 180, 180)
